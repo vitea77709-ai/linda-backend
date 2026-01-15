6387d239548541f49057e386e15d314184095a9f
vitea77709-ai <vitea77709@gmail.com>
2026-01-15 18:44:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
+import os
+import uuid
+import requests
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from openai import OpenAI
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
+
+os.makedirs("static", exist_ok=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+class ChatRequest(BaseModel):
+    message: str
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.post("/chat")
+def chat(req: ChatRequest):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Ты милая романтичная девушка по имени Линда"},
+            {"role": "user", "content": req.message}
+        ]
+    )
+
+    ai_reply = completion.choices[0].message.content
+
+    return {
+        "text": ai_reply
+    }
